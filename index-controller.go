ecb3adcd333b00ec333f4ed7af533ac33399220e
lolpez <luiyicpu@hotmail.com>
2018-12-01 22:30:43 -0400

    Added fake data for documents

--- index-controller.go
+++ index-controller.go
@@ -15,10 +15,18 @@ type User struct {
 	Email     string
 }
 
+//Document structure
+type Document struct {
+	ID   string
+	Name string
+	Size int64
+}
+
 func indexPage(writer http.ResponseWriter, r *http.Request) {
 	type WebData struct {
-		Title string
-		Users []User
+		Title     string
+		Users     []User
+		Documents []Document
 	}
 
 	data := WebData{
@@ -27,6 +35,11 @@ func indexPage(writer http.ResponseWriter, r *http.Request) {
 			{ID: 1, Name: "Lolpez", Firstname: "Luis", Lastname: "Lopez", Age: 25, Email: "luis@gmail.com"},
 			{ID: 2, Name: "Miguel", Firstname: "Miguel", Lastname: "Lopez", Age: 25, Email: "miguel@gmail.com"},
 			{ID: 3, Name: "Jose", Firstname: "Jose", Lastname: "Lopez", Age: 25, Email: "jose@gmail.com"},
+		},
+		Documents: []Document{
+			{ID: "abc123", Name: "abc.txt", Size: 50471},
+			{ID: "hdp666", Name: "hdp.js", Size: 74125},
+			{ID: "lol420", Name: "lol.jpg", Size: 853857},
 		},
 	}
 
